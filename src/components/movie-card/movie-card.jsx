76c61816e23f8388ebd0867cf94be0cb0d62a1fe
Alecks <52960889+lexivn@users.noreply.github.com>
2023-10-24 02:15:41 -0400

    Implemented the propTypes for validation
    Implemented the propTypes for validation of the data received from the parent (mainview).

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
-// export const MovieCard = (props) => {
-//   return <div>{props.movie.title}</div>;
-// };
+// There two props in this code: One object(book) and one function(onBookClick)
+
+// Here We import the PropType library
+import PropType from "prop-types";
 
 // Destructure of the props
 export const MovieCard = ({ movie, onMovieClick }) => {
@@ -10,7 +11,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         onMovieClick(movie);
       }}
     >
-      {movie.title}
+      {movie.Title}
     </div>
   );
 };
@@ -19,3 +20,12 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 //   const { movie } = props;
 //   return <div>{movie.title}</div>;
 // };
+
+MovieCard.propTypes = {
+  movie: PropType.shape({
+    Title: PropType.string.isRequired, // propType is working
+    ImagePath: PropType.string.isRequired, // propType is working
+    author: PropType.string // Why this propType is not working?
+  }).isRequired,
+  onMovieClick: PropType.func.isRequired
+};
