3aaa8ea27ccef6b55283f3886eff0df310bacbce
Alecks <52960889+lexivn@users.noreply.github.com>
2023-11-26 04:38:14 -0500

    Attemps to fix the Handle of Favorite Movie List

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -42,97 +42,97 @@ export const MovieCard = ({ token, movie }) => {
       .catch((error) => {
         console.log(error);
       })
-  
-}, [user]);
 
-// ADD MOVIE TO FAVORITE LIST
-const addToFavoriteList = () => {
-  // console.log(typeof (user));
-  // console.log(user);
-  // console.log(movie);
-  // console.log(movie._id);
-  // console.log(localStorage.getItem("token"));
+  }, [user]);
 
-  fetch(`https://moviesflix-99590597ee12.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
-    method: "POST", headers: { Authorization: `Bearer ${token}` }
-  }).then((response) => {
-    if (response.ok) {
-      alert("The movie was added to you favourite list!")
-      // console.log(user.Username);
-      // console.log(response);
-      return response.json();
-    } else {
-      console.log("Failed to update your list");
-    }
-  })
-    .then((data) => {
-      localStorage.setItem("user", JSON.stringify(data));   //   Update the LocalStorage user information
-      setUser(data);    // Update the user object with the new movie list
-      //setIsFavoriteMovies(true);
-      fetch(`https://moviesflix-99590597ee12.herokuapp.com/movie/setFavorite/${movie._id}/true`, {
-    method: "PUT", headers: { Authorization: `Bearer ${token}` }
-  }).then(() => {
+  // ADD MOVIE TO FAVORITE LIST
+  const addToFavoriteList = () => {
+    // console.log(typeof (user));
+    // console.log(user);
+    // console.log(movie);
+    // console.log(movie._id);
+    // console.log(localStorage.getItem("token"));
 
-  })
-      console.log(movie);
+    fetch(`https://moviesflix-99590597ee12.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
+      method: "POST", headers: { Authorization: `Bearer ${token}` }
+    }).then((response) => {
+      if (response.ok) {
+        alert("The movie was added to you favourite list!")
+        // console.log(user.Username);
+        // console.log(response);
+        return response.json();
+      } else {
+        console.log("Failed to update your list");
+      }
     })
-    .catch((error) => {
-      console.log(error);
-    })
-};
+      .then((data) => {
+        localStorage.setItem("user", JSON.stringify(data));   //   Update the LocalStorage user information
+        setUser(data);    // Update the user object with the new movie list
+        //setIsFavoriteMovies(true);
+        fetch(`https://moviesflix-99590597ee12.herokuapp.com/movie/setFavorite/${movie._id}/true`, {
+          method: "PUT", headers: { Authorization: `Bearer ${token}` }
+        }).then(() => {
 
-// REMOVE MOVIE FROM FAVORITE LIST
-const removeFromFavoriteList = () => {
-  // console.log(typeof (user));
-  // console.log(user);
-  // console.log(movie);
-  // console.log(movie._id);
-  // console.log(localStorage.getItem("token"));
+        })
+        console.log(movie);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  };
 
-  fetch(`https://moviesflix-99590597ee12.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
-    method: "DELETE", headers: { Authorization: `Bearer ${token}` }
-  }).then((response) => {
-    if (response.ok) {
-      alert("The movie was removed from you favourite list!")
-      console.log(user.Username);
-      console.log(response);
-      return response.json();
-    } else {
-      console.log("Failed to remove the movie from your favourite list!");
-    }
-  })
-    .then((data) => {
-      localStorage.setItem("user", JSON.stringify(data));   //   Update the LocalStorage user information
-      setUser(data);    // Update the user object with the new movie list
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-};
+  // REMOVE MOVIE FROM FAVORITE LIST
+  const removeFromFavoriteList = () => {
+    // console.log(typeof (user));
+    // console.log(user);
+    // console.log(movie);
+    // console.log(movie._id);
+    // console.log(localStorage.getItem("token"));
 
-return (
-  <Card className="h-100">
-    <Card.Img variant="top" src={movie.ImagePath} />
-    <Card.Body>
-      <Card.Title>{movie.Title}</Card.Title>
-      <Card.Text>{movie.Director.Name}</Card.Text>
-      <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-        <Button variant="link">Open</Button>
-      </Link>
-      <>
-         {movie.Featured ? (
+    fetch(`https://moviesflix-99590597ee12.herokuapp.com/users/${user.Username}/movies/${movie._id}`, {
+      method: "DELETE", headers: { Authorization: `Bearer ${token}` }
+    }).then((response) => {
+      if (response.ok) {
+        alert("The movie was removed from you favourite list!")
+        console.log(user.Username);
+        console.log(response);
+        return response.json();
+      } else {
+        console.log("Failed to remove the movie from your favourite list!");
+      }
+    })
+      .then((data) => {
+        localStorage.setItem("user", JSON.stringify(data));   //   Update the LocalStorage user information
+        setUser(data);    // Update the user object with the new movie list
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  };
+
+  return (
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director.Name}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
+        <>
+          {movie.Featured ? (
             <Button onClick={removeFromFavoriteList} variant="link">Delete from List</Button>
 
-          ) : ( 
-        <Button onClick={addToFavoriteList} variant="link">Add to my List</Button>
+          ) : (
+            <Button onClick={addToFavoriteList} variant="link">Add to my List</Button>
 
-         )
-          } 
-      </>
+          )
+          }
+        </>
 
-    </Card.Body>
-  </Card>
-);
+      </Card.Body>
+    </Card>
+  );
 };
 //  The props object is being destrucured
 // export const MovieCard = (props) => {
