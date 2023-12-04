1bdf6494c0c0cdc3c69f19f9661a066c28da4ef4
Alecks <52960889+lexivn@users.noreply.github.com>
2023-12-04 14:14:53 -0500

    Implemented the Search Bar feature
    Implemented the Search Bar feature to filter movies.

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { Link } from "react-router-dom";
 
-
-export const NavigationBar = ({ user, onLoggedOut }) => {
+export const NavigationBar = ({ user, onLoggedOut, search, setSearch }) => {
 
   return (
     <Navbar bg="light" expand="lg">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          My Flix Movie
+          MoboFlix
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -34,7 +37,18 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 </Nav.Link>
                 <Nav.Link as={Link} to="/login" onClick={onLoggedOut}>
                   Logout
-                </Nav.Link>                
+                </Nav.Link>
+                <Form className="d-flex navbar-style">
+                  <Form.Control
+                    onChange={(e) => {
+                      setSearch(e.target.value);
+                    }}
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />                  
+                </Form>
               </>
             )}
           </Nav>
