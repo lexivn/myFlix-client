1969ab10e52df05355a672c8a6a8619869026003
Alecks <52960889+lexivn@users.noreply.github.com>
2023-12-04 18:38:06 -0500

    Implementing SimilarMovies into the MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import { Link, useNavigate } from "react-router-dom";
 import { Card, Placeholder, Button, Col, Container, Row } from "react-bootstrap";
+import { SimilarMovies } from "../similar-movies/similar-movies";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
@@ -8,59 +9,37 @@ export const MovieView = ({ movies }) => {
   const movie = movies.find((b) => b._id === movieId);
   const navigate = useNavigate();
 
-  // return (
-  //   <div>
-  //     <div>
-  //       <img src={movie.ImagePath} />
-  //       {/* <img src={"data:image/jpeg;base64," + movie.ImagePath} /> */}
-  //     </div>
-  //     <div>
-  //       <span>Title: </span>
-  //       <span>{movie.Title}</span>
-  //     </div>
-  //     <div>
-  //       <span>Description: </span>
-  //       <span>{movie.Description}</span>
-  //     </div>
-  //     <div>
-  //       <span>Genre:</span>
-  //       <span>{movie.Genre.Name}</span>
-  //     </div>
-  //     <div>
-  //       <span>{movie.Genre.Description}</span>
-  //     </div>
-  //     <div>
-  //       <span>Director: </span>
-  //       <span>{movie.Director.Name}</span>
-  //     </div>
-  //     <div>
-  //       <span>Bio: </span>
-  //       <span>{movie.Director.Bio}</span>
-  //     </div>
-  //     <Link to={navigate(-1)}>
-  //       <button className="back-button" style={{ cursor: "pointer" }}>Back</button>
-  //     </Link>
-  //   </div>
-  // );
+  const similarMovies = movies.filter((m) => m.Genre.Name === movie.Genre.Name)
+
 
   return (
-    <div className="d-flex justify-content-around">
-      <Card style={{ width: '24rem' }}>
-        <Card.Img variant="top" src={movie.ImagePath} />
-        <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>
-            <p> <strong>Description: </strong>{movie.Description}</p>
-            <p> <strong>{movie.Genre.Name}: </strong>{movie.Genre.Description}</p>
-            <p> <strong>Director: </strong>{movie.Director.Name}</p>
-            <p> <strong>Biography: </strong>{movie.Director.Bio}</p>
-          </Card.Text>
-          <Link to={navigate(-1)}>
-            <Button className="back-button" style={{ cursor: "pointer" }} variant="primary">Back</Button>
-          </Link>
-
-        </Card.Body>
-      </Card>
-    </div>
+    <Container className="">
+      <Row>
+        <Col>
+          <Card className=" border-0 moviePoster mx-auto">
+            <Card.Img src={movie.ImagePath} width={100}/>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+            <Card.Body>
+              <Card.Title>{movie.Title}</Card.Title>
+              <Card.Text>
+                <p> <strong>Description: </strong>{movie.Description}</p>
+                <p> <strong>{movie.Genre.Name}: </strong>{movie.Genre.Description}</p>
+                <p> <strong>Director: </strong>{movie.Director.Name}</p>
+                <p> <strong>Biography: </strong>{movie.Director.Bio}</p>
+              </Card.Text>
+              <Link to={navigate(-1)}>
+                <Button className="back-button" style={{ cursor: "pointer" }} variant="primary">Back</Button>
+              </Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <SimilarMovies />
+      </Row>
+    </Container >
   );
 };
